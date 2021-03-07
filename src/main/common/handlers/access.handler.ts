b16f62d5e93d77a7c146e489fae348a4335386c7
Devin Don <i.inf@outlook.com>
2021-03-07 15:50:08 +0800

    perf(handlers/access): catch error

--- src/main/common/handlers/access.handler.ts
+++ src/main/common/handlers/access.handler.ts
@@ -4,13 +4,16 @@ import { AccessEntity } from '../../access';
 export class AccessHandler extends BaseHandler {
 
   async handle(next: () => Promise<any>): Promise<any> {
-    AccessEntity.insert({
-      method: this.request.method?.toUpperCase(),
-      url: this.request.url,
-      params: JSON.stringify(this.mapping.queryObject),
-      timestamp: new Date(),
-      ip: this.request.headers['x-forwarded-for'] as string || this.request.socket.remoteAddress || '1.1.1.1',
-    }).catch(error => this.rester.logger.warn(`Record log failed: ${error}`));
+    AccessEntity
+      .insert({
+        method: this.request.method?.toUpperCase(),
+        url: this.request.url,
+        params: JSON.stringify(this.mapping.queryObject),
+        timestamp: new Date(),
+        ip: this.request.headers['x-forwarded-for'] as string || this.request.socket.remoteAddress,
+        headers: this.request.headers,
+      })
+      .catch(error => this.rester.logger.warn(`Record log failed: ${error}`));
     return next();
   }
 
